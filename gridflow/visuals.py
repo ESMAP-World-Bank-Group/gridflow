86a6fb654008460a6dacd67c80db775a82d28fc2
Mohini Bariya <mohini@berkeley.edu>
2025-07-28 10:37:03 -0400

    small viz color changes

--- gridflow/visuals.py
+++ gridflow/visuals.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def country_viz(reg):
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
 
     zones_p = reg.zones.to_crs(epsg=3857)
     lines_p = reg.grid.lines.to_crs(epsg=3857)
     zones_p["centers"] = zones_p.geometry.centroid
 
-    zones_p.plot(edgecolor="black", color="lightblue", ax=ax)
-    lines_p.plot(color="pink", ax=ax)
+    zones_p.plot(edgecolor="black", color="lightgrey", linewidth=2, ax=ax)
+    lines_p.plot(color="mediumseagreen", linewidth=1, ax=ax, alpha=1)
 
     for idx, z in zones_p.iterrows():
         x, y = z.centers.x, z.centers.y
@@ -18,7 +18,7 @@ def country_viz(reg):
     # Flow model overlay
     # Get the maximum capacity in the flow model
     maxmw = np.max(np.max(reg.grid.flow))
-    maxlw = 5
+    maxlw = 9
 
     zidx = zones_p.index
     for i in zidx:
@@ -27,4 +27,5 @@ def country_viz(reg):
             cj = zones_p.centers.loc[j]
             capmw = reg.grid.flow.loc[i, j]
             plt.plot([ci.x, cj.x], [ci.y, cj.y], 
-                     linewidth=(capmw/maxmw)*maxlw)
+                     linewidth=(capmw/maxmw)*maxlw,
+                     color="darkolivegreen")
